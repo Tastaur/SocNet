607c68f54e920cf82b761690abad7a6c84e01b2a
Mark <Trrrrex93@gmail.com>
2020-05-17 21:19:22 +0400

    Fixed profilestatus

--- src/component/Profile/ProfileInfo/ProfileStatusDiscription.jsx
+++ src/component/Profile/ProfileInfo/ProfileStatusDiscription.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import classes from './ProfileInfo.module.css'
 import find from '../../../assets/images/findJob.svg'
+import unfinde from '../../../assets/images/notNeedJob.svg'
 
 const ProfileStatusDiscription = (props) => {
   return <div>
     <div>{props.profile.fullName}</div>
     <div className={classes.descriptionBlock}> {props.profile.aboutMe}</div>
-    {props.profile.lookingForAJob && <img src={find} className={classes.jobPic} alt={'i need job'}/>}
-    <div className={classes.jobStatus}>{props.profile.lookingForAJobDescription}</div>
+
+    <div className={classes.jobStatus}><span>Need job:</span> <img src={props.profile.lookingForAJob ? find : unfinde}
+                                                                   className={classes.jobPic}
+                                                                   alt={'i need job'}
+    /> {props.profile.lookingForAJobDescription}</div>
 
   </div>
 }
